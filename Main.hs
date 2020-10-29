366585329fcc043edd487c901324668c25f58921
Richard Lewis <richard@rjlewis.me.uk>
2020-10-29 21:35:10 +0000

    Add a Label and some more layout

--- Main.hs
+++ Main.hs
@@ -9,4 +9,6 @@ gui :: IO ()
 gui = do
   f <- frame [text := "Hello!"]
   q <- button f [text := "Quit", on command := close f]
-  set f [layout := widget q]
+  set f [ layout := margin 10 (column 5 [ floatCenter (label "Press")
+                                        , floatCenter (widget q)
+                                        ]) ]
